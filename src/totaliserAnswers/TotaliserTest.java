dcbcd821795692763048b550c6faf052aecfff1c
death667b <iw.daniel@student.qut.edu.au>
2016-03-16 19:01:58 +1000

    test junit

---
+++ src/totaliserAnswers/TotaliserTest.java
@@ -0,0 +1,32 @@
+package totaliserAnswers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import totaliserQuestion.Totaliser;
+
+public class TotaliserTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		/*Cant do type double
+		 * int out of range
+		 * cant do string
+		 * console.log(myTest.getSum()); (console cannot beresolved)
+		 * erros on boolean
+		 * error on null, blank
+		 */
+		
+		Totaliser myTest = new Totaliser();
+		int
+		boo = 5;
+		myTest.enterValue(boo*-1);
+		myTest.enterValue(1);
+		myTest.getSum();
+		
+		
+	}
+
+}
