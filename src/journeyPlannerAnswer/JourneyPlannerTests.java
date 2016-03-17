16f3d9abe0803d0ea32f92fb6a678b265e17547d
death667b <iw.daniel@student.qut.edu.au>
2016-03-17 10:23:38 +1000

    two problems tested and not found

--- src/journeyPlannerAnswer/JourneyPlannerTests.java
+++ src/journeyPlannerAnswer/JourneyPlannerTests.java
@@ -5,15 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import journeyPlannerQuestion.JourneyPlannerA;
+import journeyPlannerQuestion.JourneyPlannerB;
+
 public class JourneyPlannerTests {
 
-	@Before
-	public void setUp() throws Exception {
+	private JourneyPlannerA jpA;
+	private JourneyPlannerB jpB;
+	
+	@Test
+	public void jpATestZeroDistance() throws Exception {
+		jpA = new JourneyPlannerA(1);
+		assertEquals(0, jpA.journeyTime());
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void jpBTestZeroDistance() throws Exception {
+		jpB = new JourneyPlannerB(1);
+		assertEquals(0, jpB.journeyTime());
+	}
+	
+	@Test
+	public void jpATestFiveDistance() throws Exception {
+		jpA = new JourneyPlannerA(1);
+		jpA.setOrigin(0, 0);
+		jpA.setDestination(5, 0);
+		assertEquals(5, jpA.journeyTime());
 	}
 
+	@Test
+	public void jpBTestFiveDistance() throws Exception {
+		jpB = new JourneyPlannerB(1);
+		jpB.setOrigin(0, 0);
+		jpB.setDestination(5, 0);
+		assertEquals(5, jpB.journeyTime());
+	}
+	
+	
 }
