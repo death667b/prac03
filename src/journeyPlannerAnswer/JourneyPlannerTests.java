9389e6ffd4a74f3b6de2bc3eafa67cadd3b53eed
death667b <iw.daniel@student.qut.edu.au>
2016-03-17 10:32:12 +1000

    Found 1 of 2 erros

--- src/journeyPlannerAnswer/JourneyPlannerTests.java
+++ src/journeyPlannerAnswer/JourneyPlannerTests.java
@@ -14,18 +14,6 @@ public class JourneyPlannerTests {
 	private JourneyPlannerB jpB;
 	
 	@Test
-	public void jpATestZeroDistance() throws Exception {
-		jpA = new JourneyPlannerA(1);
-		assertEquals(0, jpA.journeyTime());
-	}
-
-	@Test
-	public void jpBTestZeroDistance() throws Exception {
-		jpB = new JourneyPlannerB(1);
-		assertEquals(0, jpB.journeyTime());
-	}
-	
-	@Test
 	public void jpATestFiveDistance() throws Exception {
 		jpA = new JourneyPlannerA(1);
 		jpA.setOrigin(0, 0);
@@ -41,5 +29,83 @@ public class JourneyPlannerTests {
 		assertEquals(5, jpB.journeyTime());
 	}
 	
+	@Test
+	public void jpATestZeroDistance() throws Exception {
+		jpA = new JourneyPlannerA(1);
+		assertEquals(0, jpA.journeyTime());
+	}
+
+	@Test
+	public void jpBTestZeroDistance() throws Exception {
+		jpB = new JourneyPlannerB(1);
+		assertEquals(0, jpB.journeyTime());
+	}
 	
+	@Test(expected=Exception.class)
+	public void jpATestNegitiveOriginForX() throws Exception {
+		jpA = new JourneyPlannerA(1);
+		jpB.setOrigin(-1, 0);
+	}
+
+	@Test(expected=Exception.class)
+	public void jpBTestNegitiveOriginForX() throws Exception {
+		jpB = new JourneyPlannerB(1);
+		jpB.setOrigin(-1, 0);
+	}
+	
+	@Test(expected=Exception.class)
+	public void jpATestNegitiveOriginForY() throws Exception {
+		jpA = new JourneyPlannerA(1);
+		jpB.setOrigin(0, -1);
+	}
+
+	@Test(expected=Exception.class)
+	public void jpBTestNegitiveOriginForY() throws Exception {
+		jpB = new JourneyPlannerB(1);
+		jpB.setOrigin(0, -1);
+	}
+	
+	@Test(expected=Exception.class)
+	public void jpATestNegitiveDestinationForX() throws Exception {
+		jpA = new JourneyPlannerA(1);
+		jpB.setDestination(-1, 0);
+	}
+
+	@Test(expected=Exception.class)
+	public void jpBTestNegitiveDestinationForX() throws Exception {
+		jpB = new JourneyPlannerB(1);
+		jpB.setDestination(-1, 0);
+	}
+	
+	@Test(expected=Exception.class)
+	public void jpATestNegitiveDestinationForY() throws Exception {
+		jpA = new JourneyPlannerA(1);
+		jpB.setDestination(0, -1);
+	}
+
+	@Test(expected=Exception.class)
+	public void jpBTestNegitiveDestinationForY() throws Exception {
+		jpB = new JourneyPlannerB(1);
+		jpB.setDestination(0, -1);
+	}
+	
+	@Test(expected=Exception.class)
+	public void jpATestZeroConstructor() throws Exception {
+		jpA = new JourneyPlannerA(0);
+	}
+
+	@Test(expected=Exception.class)
+	public void jpBTestZeroConstructor() throws Exception {
+		jpB = new JourneyPlannerB(0);
+	}
+	
+	@Test(expected=Exception.class)
+	public void jpATestNegitiveConstructor() throws Exception {
+		jpA = new JourneyPlannerA(-1);
+	}
+
+	@Test(expected=Exception.class)
+	public void jpBTestNegitiveConstructor() throws Exception {
+		jpB = new JourneyPlannerB(-1);
+	}
 }
