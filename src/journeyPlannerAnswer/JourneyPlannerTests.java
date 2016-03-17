0d45af463ff43a5a11d9f53637f570d1ed7077d8
death667b <iw.daniel@student.qut.edu.au>
2016-03-17 12:10:14 +1000

    moved the object JourneyPlanner jbA and jbB from global to the scope of each meathod. No errors reporting

--- src/journeyPlannerAnswer/JourneyPlannerTests.java
+++ src/journeyPlannerAnswer/JourneyPlannerTests.java
@@ -2,20 +2,16 @@ package journeyPlannerAnswer;
 
 import static org.junit.Assert.*;
 
-//import org.junit.Before;
 import org.junit.Test;
 
 import journeyPlannerQuestion.JourneyPlannerA;
 import journeyPlannerQuestion.JourneyPlannerB;
 
 public class JourneyPlannerTests {
-
-	private JourneyPlannerA jpA;
-	private JourneyPlannerB jpB;
-	
+		
 	@Test
 	public void jpATestFiveDistance() throws Exception {
-		jpA = new JourneyPlannerA(1);
+		JourneyPlannerA jpA = new JourneyPlannerA(1);
 		jpA.setOrigin(0, 0);
 		jpA.setDestination(5, 0);
 		assertEquals(5, jpA.journeyTime());
@@ -23,7 +19,7 @@ public class JourneyPlannerTests {
 
 	@Test
 	public void jpBTestFiveDistance() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		jpB.setOrigin(0, 0);
 		jpB.setDestination(5, 0);
 		assertEquals(5, jpB.journeyTime());
@@ -31,19 +27,20 @@ public class JourneyPlannerTests {
 	
 	@Test
 	public void jpATestZeroDistance() throws Exception {
+		JourneyPlannerA jpA = new JourneyPlannerA(1);
 		jpA = new JourneyPlannerA(1);
 		assertEquals(0, jpA.journeyTime());
 	}
 
 	@Test
 	public void jpBTestZeroDistance() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		assertEquals(0, jpB.journeyTime());
 	}
 	
 	@Test
 	public void jpATestTwentyDistance() throws Exception {
-		jpA = new JourneyPlannerA(1);
+		JourneyPlannerA jpA = new JourneyPlannerA(1);
 		jpA.setOrigin(0, 0);
 		jpA.setDestination(10, 10);
 		assertEquals(20, jpA.journeyTime());
@@ -51,7 +48,7 @@ public class JourneyPlannerTests {
 
 	@Test
 	public void jpBTestTwentyDistance() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		jpB.setOrigin(0, 0);
 		jpB.setDestination(10, 10);
 		assertEquals(20, jpB.journeyTime());
@@ -59,93 +56,97 @@ public class JourneyPlannerTests {
 	
 	@Test
 	public void jpATestNegitiveOriginForX() throws Exception {
-		jpA = new JourneyPlannerA(1);
+		JourneyPlannerA jpA = new JourneyPlannerA(1);
 		assertFalse(jpA.setOrigin(-1, 0));
 	}
 
 	@Test
 	public void jpBTestNegitiveOriginForX() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		assertFalse(jpB.setOrigin(-1, 0));
 	}
 	
 	@Test
 	public void jpATestNegitiveOriginForY() throws Exception {
-		jpA = new JourneyPlannerA(1);
+		JourneyPlannerA jpA = new JourneyPlannerA(1);
 		assertFalse(jpA.setOrigin(0, -1));
 	}
 
 	@Test
 	public void jpBTestNegitiveOriginForY() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		assertFalse(jpB.setOrigin(0, -1));
 	}
 	
 	@Test
 	public void jpATestNegitiveDestinationForX() throws Exception {
-		jpA = new JourneyPlannerA(1);
+		JourneyPlannerA jpA = new JourneyPlannerA(1);
 		assertFalse(jpA.setDestination(-1, 0));
 	}
 
 	@Test
 	public void jpBTestNegitiveDestinationForX() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		assertFalse(jpB.setDestination(-1, 0));
 	}
 	
 	@Test
 	public void jpATestNegitiveDestinationForY() throws Exception {
-		jpA = new JourneyPlannerA(1);
+		JourneyPlannerA jpA = new JourneyPlannerA(1);
 		assertFalse(jpA.setDestination(0, -1));
 	}
 
 	@Test
 	public void jpBTestNegitiveDestinationForY() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		assertFalse(jpB.setDestination(0, -1));
 	}
 	
 	@Test(expected=Exception.class)
 	public void jpATestZeroConstructor() throws Exception {
-		jpA = new JourneyPlannerA(0);
+		@SuppressWarnings("unused")
+		JourneyPlannerA jpA = new JourneyPlannerA(0);
 	}
 
 	@Test(expected=Exception.class)
 	public void jpBTestZeroConstructor() throws Exception {
-		jpB = new JourneyPlannerB(0);
+		@SuppressWarnings("unused")
+		JourneyPlannerB jpB = new JourneyPlannerB(0);
 	}
 	
 	@Test(expected=Exception.class)
 	public void jpATestNegitiveConstructor() throws Exception {
-		jpA = new JourneyPlannerA(-1);
+		@SuppressWarnings("unused")
+		JourneyPlannerA jpA = new JourneyPlannerA(-1);
 	}
 
 	@Test(expected=Exception.class)
 	public void jpBTestNegitiveConstructor() throws Exception {
-		jpB = new JourneyPlannerB(-1);
+		@SuppressWarnings("unused")
+		JourneyPlannerB jpB = new JourneyPlannerB(-1);
 	}
 	
 	@Test
 	public void jpATestOutOfBoundsOriginForX() throws Exception {
-		jpA = new JourneyPlannerA(1);
+		JourneyPlannerA jpA = new JourneyPlannerA(1);
 		assertFalse(jpA.setOrigin(11, 0));
 	}
 	
 	@Test
 	public void jpBTestOutOfBoundsOriginForX() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		assertFalse(jpB.setOrigin(11, 0));
 	}
 	
 	@Test
 	public void jpATestOutOfBoundsOriginForY() throws Exception {
-		jpA = new JourneyPlannerA(1);
+		JourneyPlannerA jpA = new JourneyPlannerA(1);jpA = new JourneyPlannerA(1);
 		assertFalse(jpA.setOrigin(0, 11));
 	}
 	
 	@Test
 	public void jpBTestOutOfBoundsOriginForY() throws Exception {
-		jpB = new JourneyPlannerB(1);
+		JourneyPlannerB jpB = new JourneyPlannerB(1);
 		assertFalse(jpB.setOrigin(0, 11));
 	}
 }
