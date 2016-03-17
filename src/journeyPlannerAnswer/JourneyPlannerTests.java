11db175bcfa84126ebdf290121ae78b987620815
death667b <iw.daniel@student.qut.edu.au>
2016-03-17 10:10:29 +1000

    created blank journeyPlannerTests.java

---
+++ src/journeyPlannerAnswer/JourneyPlannerTests.java
@@ -0,0 +1,19 @@
+package journeyPlannerAnswer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JourneyPlannerTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
